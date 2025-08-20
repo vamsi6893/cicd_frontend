6983b4f2406710a0921c2ce050f81533f694a9e2
vamsi6893 <nvamsiram@gmail.com>
2025-08-20 10:39:07 +0530

    first

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const BASEURL = "http://localhost:8080/cicd";
+export const BASEURL = "http://localhost:8080/cicd/";
 export function callApi(reqmethod, url, data, responseHandler)
 {
     let options = "";
